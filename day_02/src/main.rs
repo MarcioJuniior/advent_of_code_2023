91530f323b0fa04b4d4795e0375ff1277a543256
Marcio Azevedo <marcioazjunior@gmail.com>
2023-12-08 00:14:48 -0300

    Day 2 part 2

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -2,7 +2,7 @@ use regex::Regex;
 use std::collections::HashMap;
 use std::str::FromStr;
 
-#[derive(Debug, Eq, PartialEq, Hash)]
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
 enum Color {
     Red,
     Green,
@@ -42,7 +42,9 @@ impl FromStr for Play {
         let cubes = re
             .captures_iter(input)
             .map(|c| c.extract())
-            .filter_map(|(_, [number, color])| Some((number.parse().ok()?, Color::from_str(color).ok()?)))
+            .filter_map(|(_, [number, color])| {
+                Some((number.parse().ok()?, Color::from_str(color).ok()?))
+            })
             .map(|(amount, color)| Cube { color, amount })
             .collect::<Vec<_>>();
 
@@ -78,6 +80,24 @@ impl Game {
     fn is_possible(&self) -> bool {
         self.plays.iter().all(|play| self.is_possible_play(play))
     }
+
+    fn minimum_power(&self) -> usize {
+        let cubes = self.plays.iter().flat_map(|play| play.cubes.iter());
+        let mut max: HashMap<Color, usize> = HashMap::new();
+
+        for cube in cubes {
+            match max.get(&cube.color) {
+                Some(current_max) => {
+                    if cube.amount > *current_max {
+                        max.insert(cube.color, cube.amount);
+                    }
+                },
+                None => { max.insert(cube.color, cube.amount); },
+            }
+        }
+
+        max.into_values().reduce(|acc, value| acc * value).unwrap_or(0)
+    }
 }
 
 impl FromStr for Game {
@@ -86,15 +106,20 @@ impl FromStr for Game {
     fn from_str(input: &str) -> Result<Game, Self::Err> {
         let re = Regex::new(r"Game (\d+): (.+)").unwrap();
 
-        let (_, [id, plays]) = re.captures(input).map(|capture| capture.extract()).ok_or(())?;
+        let (_, [id, plays]) = re
+            .captures(input)
+            .map(|capture| capture.extract())
+            .ok_or(())?;
 
         let id = id.parse::<usize>().ok().ok_or(())?;
-        let plays = plays.split(";").filter_map(|str| Play::from_str(str).ok()).collect();
+        let plays = plays
+            .split(";")
+            .filter_map(|str| Play::from_str(str).ok())
+            .collect();
 
         Ok(Game::new(id, plays))
     }
 }
-
 #[test]
 fn part1_test() {
     let input = "
@@ -108,8 +133,22 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
     assert_eq!(part1(input), 8);
 }
 
+#[test]
+fn part2_test() {
+    let input = "
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+        ";
+
+    assert_eq!(part2(input), 2286);
+}
+
 fn part1(input: &str) -> usize {
-    input.lines()
+    input
+        .lines()
         .filter_map(|line| -> Option<Game> {
             let mut game = Game::from_str(&line).ok()?;
 
@@ -121,12 +160,18 @@ fn part1(input: &str) -> usize {
 
             Some(game)
         })
-        .filter_map(|game| {
-            match game.is_possible() {
-                true => Some(game.id),
-                false => None
-            }
+        .filter_map(|game| match game.is_possible() {
+            true => Some(game.id),
+            false => None,
         })
+        .sum()
+}
+
+fn part2(input: &str) -> usize {
+    input
+        .lines()
+        .filter_map(|line| Game::from_str(&line).ok())
+        .map(|game| game.minimum_power())
         .sum()
 }
 
@@ -134,5 +179,5 @@ fn main() {
     let input = include_str!("../input.txt");
 
     println!("Part 1: {}", part1(input));
-    //println!("Part 2: {}", result.unwrap());
+    println!("Part 2: {}", part2(input));
 }
--- day_02/target/debug/day_02.d
+++ day_02/target/debug/day_02.d
@@ -1,1 +1,1 @@
-/home/maz/Projects/advent_of_code_2023/day_02/target/debug/day_02: /home/maz/Projects/advent_of_code_2023/day_02/src/main.rs
+/home/maz/Projects/advent_of_code_2023/day_02/target/debug/day_02: /home/maz/Projects/advent_of_code_2023/day_02/input.txt /home/maz/Projects/advent_of_code_2023/day_02/src/main.rs
